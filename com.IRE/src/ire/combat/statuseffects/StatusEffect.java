00a49bcab221dcfaab4a214f1c52f4b9b442ce47
Joshua Jacobi <josh.s.jacobi@gmail.com>
2021-02-25 13:49:49 -0800

    Expanded StatusEffect framework, added jar.

--- com.IRE/src/ire/combat/statuseffects/StatusEffect.java
+++ com.IRE/src/ire/combat/statuseffects/StatusEffect.java
@@ -9,15 +9,20 @@ public abstract class StatusEffect {
     protected boolean percentage;
     protected int stacks;
     protected int duration;
-    protected int cureResilience;
+    protected int removalType;
+    protected int resilience;
 
     public StatusEffect(String name, String abbreviation, String description, boolean display, boolean percentage,
-                        int stacks, int duration) {
+                        int stacks, int duration, int removalType, int resilience) {
         this.name = name;
         this.abbreviation = abbreviation;
         this.description = description;
         this.display = display;
         this.percentage = percentage;
+        this.stacks = stacks;
+        this.duration = duration;
+        this.removalType = removalType;
+        this.resilience = resilience;
     }
 
     protected abstract boolean inflict();
